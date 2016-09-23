c7dc4390ade23b9e23c9bac203881f1714ea9f24
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2016-09-23 17:09:47 +0200

    add code to select_ref_samples to print selection of reference and predition samples in PC space

--- R/select-ref-spectra.R
+++ R/select-ref-spectra.R
@@ -16,7 +16,8 @@
 #' @usage ken_stone(spec_chem, ratio_val, pc, print = TRUE,
 #' validation = TRUE)
 #' @export
-select_ref_samples <- function(list_spectra, ratio_ref, pc = 2, print = TRUE) {
+select_ref_samples <- function(list_spectra, ratio_ref = 0.15, pc = 2,
+  print = TRUE) {
   pc_number <- eval(pc, envir = parent.frame())
   sel <- prospectr::kenStone(X = list_spectra$MIR0,
     k = round(ratio_ref * nrow(list_spectra$MIR0)), pc = substitute(pc_number))
@@ -36,9 +37,33 @@ select_ref_samples <- function(list_spectra, ratio_ref, pc = 2, print = TRUE) {
     pred_metadata = pred_metadata,
     pred_spectra = pred_spectra
   )
-  # Return sample list
+  # Plot samples selected for calibration in ggplot
+  sel_df_ref <- data.frame(sel$pc[sel$model, 1:2])
+  sel_df_ref$type <- as.factor(
+    rep("reference analysis", nrow(sel_df_cal))
+  )
+  sel_df_pred <- data.frame(sel$pc[- sel$model, 1:2])
+  sel_df_pred$type <- as.factor(
+    rep("model prediction", nrow(sel_df_pred)))
+  sel_df <- rbind(sel_df_ref, sel_df_pred)
+  # Compute ratio needed to make the figure square
+  ratio <- with(sel_df, diff(range(PC1))/diff(range(PC2)))
+  p_pca <- ggplot2::ggplot(data = sel_df) +
+    ggplot2::geom_point(
+      ggplot2::aes(x = PC1, y = PC2, shape = type), size = 4) +
+    ggplot2::coord_fixed(ratio = 1) +
+    ggplot2::scale_shape_manual(values=c(1, 19)) +
+    ggplot2::scale_colour_manual(values=c("black", "red")) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.title = ggplot2::element_blank())
+  # Print reference and prediction samples in PC1 and PC2
+  if (print == TRUE) {
+    p_pca
+  }
+  # Return sample list and ggplot object
   list(
     ref_samples = ref_samples,
-    pred_samples = pred_samples
+    pred_samples = pred_samples,
+    p_pca = p_pca
   )
 }
