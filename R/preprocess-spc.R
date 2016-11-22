ebabb2a3338dc0c79953b63925510dfdba772c80
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2016-11-22 13:29:20 +0100

    add new preprocessing options for Savitzky-Golay and Gap-segment derivatives

--- R/preprocess-spc.R
+++ R/preprocess-spc.R
@@ -16,6 +16,19 @@ preprocess_spc <- function(spc_tbl, select, column_in = "spc_mean") {
   # see e.g. Terhoeven-Urselmans et al. (2010)
   sg_1_w21 <- prospectr::savitzkyGolay(X = spc_raw,
     m = 1, p = 3, w = 21)
+  sg_1_w35 <- prospectr::savitzkyGolay(X = spc_raw,
+    m = 1, p = 3, w = 35)
+  sg_1_w51 <- prospectr::savitzkyGolay(X = spc_raw,
+    m = 1, p = 3, w = 51)
+  # Second derivative Savitzky-Golay with a window size of 21 points
+  sg_2_w21 <- prospectr::savitzkyGolay(X = spc_raw,
+    m = 2, p = 3, w = 21)
+  sg_2_w11 <- prospectr::savitzkyGolay(X = spc_raw,
+    m = 2, p = 3, w = 21)
+  # Savitzky-Golay (order 0) smoothing and derivative with a window size of
+  # 21 points
+  sg_0_1_w21 <- prospectr::savitzkyGolay(X = sg_0,
+    m = 1, p = 3, w = 21)
   # First derivative and window size of 13
   sg_1_w13 <- prospectr::savitzkyGolay(X = spc_raw,
     m = 1, p = 3, w = 13)
@@ -25,7 +38,19 @@ preprocess_spc <- function(spc_tbl, select, column_in = "spc_mean") {
   # Calculate standard normal variate (SNV) after smoothing
   sg_0_snv <- prospectr::standardNormalVariate(sg_0)
   sg_1_snv <- prospectr::standardNormalVariate(sg_1) # added 2016-08-05
-
+  # Gap-segement derivative; default option
+  gsd <- prospectr::gapDer(X = spc_raw, m = 1, w = 11, s = 10)
+  gsd_m1_w21_s21 <- prospectr::gapDer(X = spc_raw, m = 1, w = 21, s = 21)
+  gsd_m1_w21_s1 <- prospectr::gapDer(X = spc_raw, m = 1, w = 21, s = 1)
+  gsd_m1_w35_s21 <- prospectr::gapDer(X = spc_raw, m = 1, w = 35, s = 21)
+  gsd_m1_w5_s4 <- prospectr::gapDer(X = spc_raw, m = 1, w = 5, s = 4)
+  gsd_m1_w5_s21 <- prospectr::gapDer(X = spc_raw, m = 1, w = 5, s = 21)
+  # 4th Gap-segment derivative
+  gsd_m4_w21_s21 <- prospectr::gapDer(X = spc_raw, m = 4, w = 21, s = 21)
+  # 2nd Gap-segment derivative
+  gsd_m2_w21_s21 <- prospectr::gapDer(X = spc_raw, m = 2, w = 21, s = 21)
+  # Continuum-removal
+  cr <- prospectr::continuumRemoval(X = spc_raw, type = "A")
   # Select final preprocessing based on selection argument and
   # save matrix in data table
   pre <- select
