7c283a88557358de1119f53ec621d69859290005
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2016-11-29 09:49:08 +0100

    extend the options for preprocessing methods and combinations of parameters

--- R/preprocess-spc.R
+++ R/preprocess-spc.R
@@ -8,12 +8,16 @@ preprocess_spc <- function(spc_tbl, select, column_in = "spc_mean") {
   spc_raw <- data.table::rbindlist(spc_tbl[column_in][[column_in]])
 
   # Perform preprocessing
-  sg_0 <- prospectr::savitzkyGolay(X = spc_raw,
+  sg_0_w9 <- prospectr::savitzkyGolay(X = spc_raw,
     m = 0, p = 3, w = 9) # smoothing and averaging
-  sg_1 <- prospectr::savitzkyGolay(X = spc_raw,
+  sg_1_w5 <- prospectr::savitzkyGolay(X = spc_raw,
     m = 1, p = 3, w = 5) # first derivative ***
   # Implement window size of 21, corresponds to ICRAF standard;
   # see e.g. Terhoeven-Urselmans et al. (2010)
+  sg_1_w11 <- prospectr::savitzkyGolay(X = spc_raw,
+    m = 1, p = 3, w = 11)
+  sg_1_w15 <- prospectr::savitzkyGolay(X = spc_raw,
+    m = 1, p = 3, w = 15)
   sg_1_w21 <- prospectr::savitzkyGolay(X = spc_raw,
     m = 1, p = 3, w = 21)
   sg_1_w35 <- prospectr::savitzkyGolay(X = spc_raw,
@@ -24,7 +28,7 @@ preprocess_spc <- function(spc_tbl, select, column_in = "spc_mean") {
   sg_2_w21 <- prospectr::savitzkyGolay(X = spc_raw,
     m = 2, p = 3, w = 21)
   sg_2_w11 <- prospectr::savitzkyGolay(X = spc_raw,
-    m = 2, p = 3, w = 21)
+    m = 2, p = 3, w = 11)
   # Savitzky-Golay (order 0) smoothing and derivative with a window size of
   # 21 points
   sg_0_1_w21 <- prospectr::savitzkyGolay(X = sg_0,
@@ -36,8 +40,20 @@ preprocess_spc <- function(spc_tbl, select, column_in = "spc_mean") {
   sg_2 <- prospectr::savitzkyGolay(X = spc_raw,
     m = 2, p = 3, w = 5)
   # Calculate standard normal variate (SNV) after smoothing
-  sg_0_snv <- prospectr::standardNormalVariate(sg_0)
-  sg_1_snv <- prospectr::standardNormalVariate(sg_1) # added 2016-08-05
+  sg_0_snv <- prospectr::standardNormalVariate(sg_0_w9)
+  sg_1_snv <- prospectr::standardNormalVariate(sg_1_w5) # added 2016-08-05
+  # Standard normal variate (SNV) and first gap-segment derivative
+  snv_gsd_m1_w11_s1 <- prospectr::gapDer(X = sg_0_snv, m = 1, w = 11, s = 1)
+  snv_gsd_m1_w21_s5 <- prospectr::gapDer(X = sg_0_snv, m = 1, w = 21, s = 5)
+  snv_gsd_m1_w31_s1 <- prospectr::gapDer(X = sg_0_snv, m = 1, w = 31, s = 5)
+  snv_gsd_m1_w31_s5 <- prospectr::gapDer(X = sg_0_snv, m = 1, w = 31, s = 5)
+  # Standard normal variate (SNV) and second gap-segment derivative
+  snv_gsd_m2_w21_s1 <- prospectr::gapDer(X = sg_0_snv, m = 2, w = 21, s = 1)
+  snv_gsd_m2_w31_s1 <- prospectr::gapDer(X = sg_0_snv, m = 2, w = 31, s = 5)
+  snv_gsd_m2_w31_s5 <- prospectr::gapDer(X = sg_0_snv, m = 2, w = 31, s = 1)
+  snv_gsd_m2_w51_s1 <- prospectr::gapDer(X = sg_0_snv, m = 2, w = 51, s = 1)
+  snv_gsd_m2_w51_s5 <- prospectr::gapDer(X = sg_0_snv, m = 2, w = 51, s = 5)
+  snv_gsd_m2_w5_s1 <- prospectr::gapDer(X = sg_0_snv, m = 2, w = 5, s = 1)
   # Gap-segement derivative; default option
   gsd <- prospectr::gapDer(X = spc_raw, m = 1, w = 11, s = 10)
   gsd_m1_w21_s21 <- prospectr::gapDer(X = spc_raw, m = 1, w = 21, s = 21)
@@ -45,6 +61,8 @@ preprocess_spc <- function(spc_tbl, select, column_in = "spc_mean") {
   gsd_m1_w35_s21 <- prospectr::gapDer(X = spc_raw, m = 1, w = 35, s = 21)
   gsd_m1_w5_s4 <- prospectr::gapDer(X = spc_raw, m = 1, w = 5, s = 4)
   gsd_m1_w5_s21 <- prospectr::gapDer(X = spc_raw, m = 1, w = 5, s = 21)
+  gsd_m1_w11_s21 <- prospectr::gapDer(X = spc_raw, m = 1, w = 11, s = 21)
+  gsd_m1_w11_s5 <- prospectr::gapDer(X = spc_raw, m = 1, w = 11, s = 5)
   # 4th Gap-segment derivative
   gsd_m4_w21_s21 <- prospectr::gapDer(X = spc_raw, m = 4, w = 21, s = 21)
   # 2nd Gap-segment derivative
