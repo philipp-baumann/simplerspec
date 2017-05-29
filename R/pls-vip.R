1caef6d7083554eec1b87ebf6663e9ae29bec4da
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2017-05-29 18:00:58 +0200

    Add custom plotting function `plot_pls_vip()` and helper function `create_vip_rects()`. The plotting function draws stacked graphs of VIP for the final PLS regression model of the calibration (training) data set for the final number of components, raw (replicate mean) spectra, and preprocessed spectra.

--- R/pls-vip.R
+++ R/pls-vip.R
@@ -57,6 +57,7 @@ VIPjh <- function(object, j, h) {
     sqrt(nrow(W) * sum(SS * W[j,]^2 / Wnorm2) / sum(SS))
 }
 
+
 #' @export
 extract_pls_vip <- function(mout) {
   # Compute VIP for all wavenumbers and select only VIPs with ncomp in final
@@ -69,4 +70,133 @@ extract_pls_vip <- function(mout) {
   tibble::data_frame_(lazyeval::lazy_dots(wavenumber = wn, vip = vip))
 }
 
+
+#' @export
+plot_pls_vip <- function(mout, y1 = "spc_mean", y2 = "spc_pre",
+                         by = "sample_id",
+                         xlab = expression(paste("Wavenumber [", cm^-1, "]")),
+                         ylabs = c("Absorbance", "Preprocessed Abs."),
+                         alpha = 0.2) {
+
+  # Extract spectra tibble for calibration
+  spc_tbl <- mout$data$calibration
+  # Gather spectra in one data.table
+
+  ifelse(y1 == "spc",
+    {dt1 <- data.table::data.table(do.call(rbind, spc_tbl[, y1][[y1]]))},
+    {dt1 <- data.table::rbindlist(spc_tbl[, y1][[y1]])}
+  )
+
+  ifelse(y2 == "spc",
+    {dt2 <- data.table::data.table(do.call(rbind, spc_tbl[, y2][[y2]]))},
+    {dt2 <- data.table::rbindlist(spc_tbl[, y2][[y2]])}
+  )
+
+  # Extract ID variable and append it to the data.table
+  id <- spc_tbl[, by][[by]]
+  dt1[, id := id]
+  dt2[, id := id]
+
+  # Convert data table to long form for ggplot2 plotting
+  dt1_long <- data.table::melt(
+    dt1, measure = names(dt1)[!names(dt1) %in% c("id")]
+  )
+  dt1_long[, variable := as.numeric(as.character(variable))]
+  data.table::setnames(dt1_long, old = "variable", new = "wavenumber")
+
+  dt2_long <- data.table::melt(
+    dt2, measure = names(dt2)[!names(dt2) %in% c("id")]
+  )
+  dt2_long[, variable := as.numeric(as.character(variable))]
+  data.table::setnames(dt2_long, old = "variable", new = "wavenumber")
+
+  # Plot spectra and VIP scores
+  brk <- pretty(as.numeric(names(dt1)[!names(dt1) %in% c("id")]),
+    n = 10)
+  maxmin <- function(x) {c(max(x), min(x))}
+  x_lim <- maxmin(as.numeric(names(dt1)[!names(dt1) %in% c("id")]))
+
+  # Extract VIP (Variable Importance in Projection) scores for the given
+  # model
+  df_vip <- extract_pls_vip(mout)
+
+  # Determine highlighted regions above VIP = 1
+  rects <- create_vip_rects(df_vip)
+
+  # Plot for resampled and mean replicate spectra
+  p_spc <- ggplot2::ggplot(dt1_long, ggplot2::aes(wavenumber, value)) +
+    ggplot2::geom_rect(data = rects, inherit.aes = FALSE,
+      ggplot2::aes(xmin = start, xmax = end, ymin = min(dt1_long$value),
+        ymax = max(dt1_long$value), group = group), color = "transparent",
+        fill = "orange", alpha = 0.3) +
+    ggplot2::geom_line(ggplot2::aes(group = id), alpha = alpha, size = 0.2) +
+    ggplot2::labs(x = xlab, y = ylabs[1]) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(plot.margin = ggplot2::unit(c(0, 5, 1, 1),
+      units = "points")) +
+    ggplot2::scale_x_reverse(limits = x_lim, breaks = brk)
+
+    p_spc_pre <- ggplot2::ggplot(dt2_long, ggplot2::aes(wavenumber, value)) +
+    ggplot2::geom_rect(data = rects, inherit.aes = FALSE,
+      ggplot2::aes(xmin = start, xmax = end, ymin = min(dt2_long$value),
+        ymax = max(dt2_long$value), group = group), color = "transparent",
+        fill = "orange", alpha = 0.3) +
+    ggplot2::geom_line(ggplot2::aes(group = id), alpha = alpha, size = 0.2) +
+    ggplot2::labs(x = xlab, y = ylabs[2]) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(plot.margin = ggplot2::unit(c(0, 5, 1, 1),
+      units = "points")) +
+    ggplot2::scale_x_reverse(limits = x_lim, breaks = brk) +
+    ggplot2::theme(plot.margin = ggplot2::unit(c(1, 5, -30, 6),
+      units = "points"),
+      axis.title.y = ggplot2::element_text(vjust = 0.25),
+      axis.text.x = ggplot2::element_blank())
+
+  # Plot for VIP
+  # Extract PLS model response variable and number of components
+  response <- as.character(unique(mout$stats$variable))
+  ncomp <- mout$pls_model$finalModel$ncomp
+  p_vip <- ggplot2::ggplot(data = df_vip,
+      ggplot2::aes(x = wavenumber, y = vip)) +
+    ggplot2::geom_rect(data = rects, inherit.aes = FALSE,
+      ggplot2::aes(xmin = start, xmax = end, ymin = min(df_vip$vip),
+      ymax = max(df_vip$vip), group = group), color = "transparent",
+      fill = "orange", alpha = 0.3) +
+    ggplot2::geom_hline(yintercept = 1, colour = "red") +
+    ggplot2::geom_line() +
+    ggplot2::ylab(
+      bquote(paste(VIP[PLSR], " (", .(response),
+        ", ", .(ncomp), " comps)"))) +
+    ggplot2::scale_x_reverse(limits = x_lim, breaks = brk) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(plot.margin = ggplot2::unit(c(1, 5, -30, 6),
+      units = "points"),
+      axis.title.y = ggplot2::element_text(vjust = 0.25),
+    axis.text.x = ggplot2::element_blank())
+
+  # Arrange plots in two panels without any margins in between
+  # Hints from
+  # https://stackoverflow.com/questions/42567045/alignment-of-two-plots-using-grid-arrange
+  cowplot::plot_grid(
+    p_vip, p_spc_pre, p_spc, rel_heights = c(0.3, 0.3, 0.4),
+    ncol = 1, align = "v")
+}
+
+#' @export
+create_vip_rects <- function(df_vip) {
+
+  # Highlight region data
+  # https://stackoverflow.com/questions/32543176/highlight-areas-within-certain-x-range-in-ggplot2
+  v <- rep(0, nrow(df_vip))
+  v[df_vip$vip > 1] <- 1
+  # Get the start and end points for highlighted regions
+  inds <- diff(c(0, v))
+  start <- df_vip$wavenumber[inds == 1]
+  end <- df_vip$wavenumber[inds == -1]
+  if (length(start) > length(end)) {
+    end <- c(end, tail(df_vip$wavenumber, 1))
+  }
+  # Create data frame for rectangle layer (geom_rects)
+  data.frame(start = start, end = end, group = seq_along(start))
+}
 
