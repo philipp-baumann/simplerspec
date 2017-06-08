60ffd79829a85a84dc5d96476dd4ef79f21ef305
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2017-06-08 07:51:09 +0200

    iterate by position index (`seq_along` instead of name because `wavenumbers` and `spc` (spectra) list-columns might accidently have different list names. By position is saver

--- R/resample-spc.R
+++ R/resample-spc.R
@@ -44,7 +44,7 @@ resample_spc <- function(spc_tbl, x_unit = "wavenumber",
     wn_lower = 510, wn_upper = 3988, wn_interval = 2,
     wl_lower = 350, wl_upper = 2500, wl_interval = 1) {
 
-  if (x_unit == "wavenumber" & "wavenumbers" %in% names(spc_tbl)) {
+  if (x_unit == "wavenumber" && "wavenumbers" %in% names(spc_tbl)) {
     # Create sequence of new wavenumbers
     wn_seq <- rev(seq(from = wn_lower, wn_upper, by = wn_interval))
 
@@ -53,11 +53,11 @@ resample_spc <- function(spc_tbl, x_unit = "wavenumber",
     names(wavenumbers_rs) <- names(spc_tbl$spc)
 
     # Resample all spectra in list column spc using prospectr
-    spc_rs <- lapply(names(spc_tbl$spc), function(nm) {
+    spc_rs <- lapply(seq_along(spc_tbl$spc), function(i) {
       data.table::data.table(
         prospectr::resample(
-          X = spc_tbl$spc[[nm]], # spectral matrix to resample
-          wav = spc_tbl$wavenumbers[[nm]], # old wavenumbers
+          X = spc_tbl$spc[[i]], # spectral matrix to resample
+          wav = spc_tbl$wavenumbers[[i]], # old wavenumbers
           new.wav = wn_seq # new wavenumbers
         )
       )
@@ -71,7 +71,7 @@ resample_spc <- function(spc_tbl, x_unit = "wavenumber",
       wavenumbers_rs = wavenumbers_rs
     )
 
-  } else if (x_unit == "wavelength" & "wavelengths" %in% names(spc_tbl)) {
+  } else if (x_unit == "wavelength" && "wavelengths" %in% names(spc_tbl)) {
     # Create sequence of new wavelengths
     wl_seq <- rev(seq(from = wl_lower, wl_upper, by = wl_interval))
 
@@ -80,11 +80,11 @@ resample_spc <- function(spc_tbl, x_unit = "wavenumber",
     names(wavelengths_rs) <- names(spc_tbl$spc)
 
     # Resample all spectra in list column spc using prospectr
-    spc_rs <- lapply(names(spc_tbl$spc), function(nm) {
+    spc_rs <- lapply(seq_along(spc_tbl$spc), function(nm) {
       data.table::data.table(
         prospectr::resample(
-          X = spc_tbl$spc[[nm]], # spectral matrix to resample
-          wav = spc_tbl$wavelengths[[nm]], # old wavenumbers
+          X = spc_tbl$spc[[i]], # spectral matrix to resample
+          wav = spc_tbl$wavelengths[[i]], # old wavenumbers
           new.wav = wl_seq # new wavenumbers
         )
       )
