2056d04cf65c629a0ec8598984933df6c61f1ec3
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2017-06-15 12:14:37 +0200

    correct degrees of freedom by minius 1 when calculating standard error of the mean 95% confidence interval

--- R/spectra-utils.R
+++ R/spectra-utils.R
@@ -35,7 +35,7 @@ summary_df <- function(df, x, y){
 #' @export
 # Function to calculate standard error of the mean
 sem <- function(x) {
-  qt(0.975, df = length(na.omit(x))) *
+  qt(0.975, df = length(na.omit(x)) - 1) *
     sqrt(var(x, na.rm = TRUE) / length(na.omit(x)))
 }
 #' @export
