f4ffa9da03849b2d795a38d337372ce86b04ed6d
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2017-11-11 11:46:21 +0100

    Add functions to slice spectra into new x unit ranges; works on spectral tibble data structures

---
+++ R/slice-spc.R
@@ -0,0 +1,5 @@
+################################################################################
+## Functions to slice spectra into a set of x axis ranges. A subset of the
+## spectra stored in a list-column of a spectral tibble will be selected and
+## the x unit axis vector will be reduced accordingly
+################################################################################
