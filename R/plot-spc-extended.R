f85f4308a2aa205f4259f6a68de62ee25a5ba23f
philipp-baumann <philipp.baumann@usys.ethz.ch>
2018-07-31 16:33:35 +0200

    Fix bug when `spc_tbl_l` supplied and `group_id_order` = TRUE (`spc_tbl` not found)

--- R/plot-spc-extended.R
+++ R/plot-spc-extended.R
@@ -389,8 +389,10 @@ plot_spc_ext <- function(spc_tbl, spc_tbl_l = NULL,
       spc_id = spc_id, group_id = group_id)
   }
   # Option to order originally numeric group_id factors by group
-  if (group_id_order && is.numeric(dplyr::pull(spc_tbl, !!group_id))) {
+  if (!is.null(spc_tbl_l)) {
+    if (group_id_order && is.numeric(dplyr::pull(spc_tbl, !!group_id))) {
     dt <- reorder_factor_num(dt = dt, column = "group_id")
+    }
   }
   brk <- pretty(dt[["xvalues_value"]], n = 10) # Pretty x-axis breaks
   p <- ggplot2::ggplot(data = dt,
