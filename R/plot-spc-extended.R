b58ab4af4c0fd6a25e77e2dce839f0802fe6ce1b
philipp-baumann <philipp.baumann@usys.ethz.ch>
2018-08-08 16:32:19 +0200

    Fix error "(list) object cannot be coerced to type double"

--- R/plot-spc-extended.R
+++ R/plot-spc-extended.R
@@ -272,13 +272,9 @@ merge_dts_l <- function(spc_tbl_l,
 # ------------------------------------------------------------------------------
 reorder_factor_num <- function(dt, column = "group_id") {
   group_id <- NULL
-  if (!any(is.na(
-    suppressWarnings(as.numeric(dt[, column, with = FALSE]))))
-  ) {
-    dt[, group_id := as.factor(group_id)]
-    sorted_labels <- paste(sort(as.numeric(levels(dt$group_id))))
-    dt$group_id <- factor(dt$group_id, levels = sorted_labels)
-  }
+  dt[, group_id := as.factor(group_id)]
+  sorted_labels <- paste(sort(as.numeric(levels(dt$group_id))))
+  dt$group_id <- factor(dt$group_id, levels = sorted_labels)
   dt
 }
 
