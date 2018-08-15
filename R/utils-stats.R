a3b04887ab10db385e6bed05c8794fe3b9e24941
philipp-baumann <philipp.baumann@usys.ethz.ch>
2018-08-15 14:45:42 +0200

    Add descriptive measures of observed

--- R/utils-stats.R
+++ R/utils-stats.R
@@ -11,22 +11,31 @@ summary_df <- function(df, x, y){
   y <- df[, y]
   b <- lm(x ~ y)$coefficients[2]
   data.frame(
-    rmse = sqrt(sum((x - y)^2, na.rm = T) / length(x)),
+    rmse = sqrt(sum((x - y)^2, na.rm = TRUE) / length(x)),
     rmsd = mean((y - x)^2)^.5,
     msd = mean((y - x)^2),
-    sdev = sd(x, na.rm = T),
-    rpd =  sd(x,na.rm = T) /
-      sqrt(sum((y - x)^2, na.rm = T) / (length(x) - 1)),
-    rpiq = (quantile(x, .75, na.rm = T) - quantile(x, .25, na.rm = T)) /
-      sqrt(sum((x - y)^2, na.rm = T) / (length(x) - 1)),
+    min = min(x, ra.rm = TRUE),
+    max = max(x, na.rm = TRUE),
+    mean = mean(x, na.rm = TRUE),
+    median = median(x, na.rm = TRUE),
+    sdev = sd(x, na.rm = TRUE),
+    rpd = sd(x, na.rm = TRUE) /
+      sqrt(sum((y - x)^2, na.rm = TRUE) / (length(x) - 1)),
+    rpiq = (quantile(x, .75, na.rm = TRUE) - quantile(x, .25, na.rm = TRUE)) /
+      sqrt(sum((x - y)^2, na.rm = TRUE) / (length(x) - 1)),
     r2  = cor(x, y, use = "pairwise.complete.obs")^2,
-    bias  = mean(y, na.rm = T) - mean(x, na.rm = T),
-    SB = (mean(y, na.rm = T) - mean(x, na.rm = T))^2,
+    bias  = mean(y, na.rm = TRUE) - mean(x, na.rm = TRUE),
+    SB = (mean(y, na.rm = TRUE) - mean(x, na.rm = TRUE))^2,
     NU = mean((y - mean(y))^2) * (1 - lm(x ~ y)$coefficients[2])^2,
-    LC = mean((x - mean(x))^2) * (1 - cor(x, y, use = "pairwise.complete.obs")^2),
-    SB_prop = round((mean(y, na.rm = T) - mean(x, na.rm = T))^2 / mean((y - x)^2) * 100, 0),
-    NU_prop = round(mean((y - mean(y))^2) * (1 - lm(x ~ y)$coefficients[2])^2 / mean((y - x)^2) * 100, 0),
-    LC_prop = round(mean((x - mean(x))^2) * (1 - cor(x, y, use = "pairwise.complete.obs")^2) / mean((y - x)^2) * 100, 0),
+    LC = mean((x - mean(x))^2)
+      * (1 - cor(x, y, use = "pairwise.complete.obs")^2),
+    SB_prop = round((mean(y, na.rm = TRUE) - mean(x, na.rm = TRUE))^2
+      / mean((y - x)^2) * 100, 0),
+    NU_prop = round(mean((y - mean(y))^2) * (1 - lm(x ~ y)$coefficients[2])^2
+      / mean((y - x)^2) * 100, 0),
+    LC_prop = round(mean((x - mean(x))^2)
+      * (1 - cor(x, y, use = "pairwise.complete.obs")^2)
+      / mean((y - x)^2) * 100, 0),
     n = length(x),
     b = b
   )
