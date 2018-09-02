0493fc7bf3d7e1c498a906b4e6c8fea6456f85f1
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2018-09-02 18:29:51 +0200

    `evaluate_model()` is renamed model evalution function that replaces `summary_df`, which is now avaiable as wrapper to ensure backward compatibility

--- R/utils-stats.R
+++ R/utils-stats.R
@@ -1,9 +1,12 @@
 #' @title Calculate model evaluation metrics
-#' @description Calculates model statistics for predicted (y)
-#' vs. observed (y) values
-#' @param df data.frame with predicted and observed data
-#' @param x column with observed values
-#' @param y column with predicted values
+#' @description Calculate observed summary statistics and model evaluation
+#' statistics for assessing agreement between observed (`obs`) and
+#' predicted (`pred`) values.
+#' @param data `data.frame`` with predicted and observed data in columns.
+#' @param obs Column that contains observed values, `symbol`/`name` or
+#' `character` (wrapped in "").
+#' @param pred Column that contains predicted values, `symbol`/`name` or
+#' `character` (wrapped in "").
 #' @importFrom e1071 kurtosis
 #' @export
 
@@ -21,67 +24,84 @@
 # regression parameters; namely, these are squared bias (SB),
 # non-unity slope (NU) and lack of correlation (LC)
 
-summary_df <- function(df, x, y) {
-  # !!! note that y are predicted values and x are observed values
-  x <- rlang::enquo(x)
-  y <- rlang::enquo(y)
-  x <- dplyr::pull(df, !!x)
-  y <- dplyr::pull(df, !!y)
+evaluate_model <- function(data, obs, pred) {
+  # Implement quasiquotation to first quote the `obs` and `pred` arguments, and
+  # unquote/evaluate in the context of the `data` data.frame;
+  # obs` and `pred` can be # both symbols or character;
+  # `obs = obs` or `obs = "obs"` when the column that contains observed values
+  # is named "obs"
+  obs <- rlang::enquo(obs)
+  pred <- rlang::enquo(pred)
+  obs <- dplyr::pull(data, !!obs)
+  pred <- dplyr::pull(data, !!pred)
 
   tibble::tibble(
     ## Compute descriptive statistics of the observations/measurements
-    n = length(x),
-    min = min(x, ra.rm = TRUE),
-    max = max(x, na.rm = TRUE),
-    mean = mean(x, na.rm = TRUE),
-    median = median(x, na.rm = TRUE),
-    sdev = sd(x, na.rm = TRUE),
-    cv = sd(x, na.rm = TRUE) / mean(x, na.rm = TRUE),
-    skewness_b1 = e1071::skewness(x, na.rm = TRUE, type = 3),
-    kurtosis = e1071::kurtosis(x, na.rm = TRUE),
+    n = length(obs),
+    min = min(obs, ra.rm = TRUE),
+    max = max(obs, na.rm = TRUE),
+    mean = mean(obs, na.rm = TRUE),
+    median = median(obs, na.rm = TRUE),
+    sdev = sd(obs, na.rm = TRUE),
+    cv = sd(obs, na.rm = TRUE) / mean(obs, na.rm = TRUE),
+    skewness_b1 = e1071::skewness(obs, na.rm = TRUE, type = 3),
+    kurtosis = e1071::kurtosis(obs, na.rm = TRUE),
 
     ## Compute model evaluation measures to address different aspects
     ## of how well predictions correspond to observations/measurements
     # Root mean squared error
-    rmse = mean((x - y)^2, na.rm = TRUE)^.5,
+    rmse = mean((obs - pred)^2, na.rm = TRUE)^.5,
     # Mean squared error; mse^2 = me^2 + msv = me^2 + sde^2
-    mse = mean((x - y)^2, na.rm = TRUE),
+    mse = mean((obs - pred)^2, na.rm = TRUE),
     # Terms mean error (ME) and bias are equivalent
-    me = mean(x, na.rm = TRUE) - mean(y, na.rm = TRUE),
-    bias = mean(x, na.rm = TRUE) - mean(y, na.rm = TRUE),
+    me = mean(obs, na.rm = TRUE) - mean(pred, na.rm = TRUE),
+    bias = mean(obs, na.rm = TRUE) - mean(pred, na.rm = TRUE),
     # Mean squared variation (of the error); difference between the simulation
     # and the measurement with respect to the deviation from the means
-    msv = mean(((mean(x, na.rm = TRUE) - x) - (mean(y, na.rm = TRUE) - y))^2),
+    msv = mean(((mean(obs, na.rm = TRUE) - obs)
+      - (mean(pred, na.rm = TRUE) - pred))^2),
     # Standard deviation of the error := SDE = MSV^0.5
-    sde = mean(((mean(x, na.rm = TRUE) - x)
-      - (mean(y, na.rm = TRUE) - y))^2)^0.5,
+    sde = mean(((mean(obs, na.rm = TRUE) - obs)
+      - (mean(pred, na.rm = TRUE) - pred))^2)^0.5,
     # Mean absolute error
-    mae = mean(abs(x - y), na.rm = TRUE),
-    r2  = cor(x, y, use = "pairwise.complete.obs")^2,
-    b = lm(x ~ y)$coefficients[2],
+    mae = mean(abs(obs - pred), na.rm = TRUE),
+    r2  = cor(obs, pred, use = "pairwise.complete.obs")^2,
+    b = lm(obs ~ y)$coefficients[2],
     # Ratio of performance to deviation
-    rpd = sd(x, na.rm = TRUE) /
-      sqrt(sum((x - y)^2, na.rm = TRUE) / (length(x) - 1)),
+    rpd = sd(obs, na.rm = TRUE) /
+      sqrt(sum((obs - pred)^2, na.rm = TRUE) / (length(obs) - 1)),
     # Ratio of performance to interquartile range
-    rpiq = (quantile(x, .75, na.rm = TRUE) - quantile(x, .25, na.rm = TRUE)) /
-      sqrt(sum((x - y)^2, na.rm = TRUE) / (length(x) - 1)),
+    rpiq = (quantile(obs, .75, na.rm = TRUE) - quantile(obs, .25, na.rm = TRUE))
+     / sqrt(sum((obs - pred)^2, na.rm = TRUE) / (length(obs) - 1)),
     # See Gauch et. al., 2003) for MSD decomposition into SB, NU and LC;
     # Squared bias
-    SB = (mean(x - y, na.rm = TRUE))^2,
+    SB = (mean(obs - pred, na.rm = TRUE))^2,
     # Non-unity slope
-    NU = mean((y - mean(y))^2) * (1 - lm(x ~ y)$coefficients[2])^2,
+    NU = mean((pred - mean(pred))^2) * (1 - lm(obs ~ y)$coefficients[2])^2,
     # Lack of correlation
-    LC = mean((x - mean(x))^2)
-      * (1 - cor(x, y, use = "pairwise.complete.obs")^2),
+    LC = mean((obs - mean(obs))^2)
+      * (1 - cor(obs, pred, use = "pairwise.complete.obs")^2),
     # Proportional contributions of SB, NU and LC to MSE in percent
-    SB_prop = round((mean(y, na.rm = TRUE) - mean(x, na.rm = TRUE))^2
-      / mean((y - x)^2) * 100, 0),
-    NU_prop = round(mean((y - mean(y))^2) * (1 - lm(x ~ y)$coefficients[2])^2
-      / mean((y - x)^2) * 100, 0),
-    LC_prop = round(mean((x - mean(x))^2)
-      * (1 - cor(x, y, use = "pairwise.complete.obs")^2)
-      / mean((y - x)^2) * 100, 0)
+    SB_prop = round((mean(pred, na.rm = TRUE) - mean(obs, na.rm = TRUE))^2
+      / mean((pred - obs)^2) * 100, 0),
+    NU_prop = round(mean((pred - mean(pred))^2)
+      * (1 - lm(obs ~ y)$coefficients[2])^2 / mean((pred - obs)^2) * 100, 0),
+    LC_prop = round(mean((obs - mean(obs))^2)
+      * (1 - cor(obs, pred, use = "pairwise.complete.obs")^2)
+      / mean((pred - obs)^2) * 100, 0)
   )
+}
+
+# Wrapper function to ensure compatibility with old summary_df() function
+# !!! note that y are predicted values and x are observed values;
+# this is only for backward compatibility;
+# This deviates from the principles that observed (O) should be denoted in the
+# in the y-axis vs. predicted (P) in the x-axis ((OP) regressions), according
+# Pinẽiro et al. (2008)
+#' @rdname evaluate_model
+#' @export
+summary_df <- function(df, x, y) {
+  evaluate_model(data = df, obs = x, pred = y)
 }
 
 # Function to calculate standard error of the mean
