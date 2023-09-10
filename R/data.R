338493799ca4c1370523408952a7d6808ff20dd9
Philipp Baumann <baumann-philipp@proton.me>
2023-09-10 22:09:05 +0200

    document newly added data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Soil spectra and laboratory reference data from Baumann et al. (2021)
+#'
+#' Data from "Estimation of soil properties with mid-infrared soil spectroscopy
+#' across yam production landscapes in West Africa".
+#'
+#' @format ## `soilspec_yamsys`
+#' A tibble data frame with 284 rows and 40 columns. The spectra are in the
+#' `spc` list-column.
+#' @source <https://soil.copernicus.org/articles/7/717/2021/>
+"soilspec_yamsys"
